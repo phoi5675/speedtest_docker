7a820bd9992638be3f14f88ae37ca4681223cbcc
phoi5675 <phoi5675@gmail.com>
2022-01-28 21:26:17 +0900

    add argparse

--- plot/plot.py
+++ plot/plot.py
@@ -1,6 +1,8 @@
 import os
 import json
 import matplotlib.pyplot as plt
+import argparse
+import csv
 from datetime import datetime, timedelta
 from scipy.ndimage import gaussian_filter1d
 
@@ -9,12 +11,38 @@ LOGS_DIR = '..\\logs\\' if os.name == 'nt' else '../logs/'
 local_format = "%m-%d-%H:%M"
 KST = +9
 
+parser = argparse.ArgumentParser(description='plot network speed graph based on speedtest.net result')
+args = None
+
+
+def make_parser() -> argparse.Namespace:
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='verbose mode')
+    parser.add_argument('--csv', action='store_true',
+                        help='save result as csv file')
+    parser.add_argument('--name', '-n', type=str, default='result.csv',
+                        help='csv file will be saved as this argument string(default: result.csv)')
+
+    return parser.parse_args()
+
+
+def save_as_file(date: list, up_speed: list, down_speed: list) -> None:
+    filename = args.name if '.csv' in args.name else args.name + '.csv'
+
+    with open(filename, 'w', newline='') as file_writer:
+        csv_writer = csv.writer(file_writer)
+
+        # write header
+        csv_writer.writerow(['date', 'upload_speed(Mbps)', 'download_speed(Mbps)'])
+        for (d, up, down) in zip(date, up_speed, down_speed):
+            csv_writer.writerow([d, up, down])
+
 
 def read_logs() -> list:
     files = os.listdir(LOGS_DIR)
-    jsons = [json for json in files if json.__str__().__contains__("json")]
+    _jsons = [_json for _json in files if _json.__str__().__contains__("json")]
 
-    return jsons
+    return _jsons
 
 
 def read_jsons() -> list:
@@ -24,7 +52,8 @@ def read_jsons() -> list:
             with open(LOGS_DIR + str(json_file), 'r') as j_file:
                 _jsons.append(json.load(j_file))
         except Exception as ex:
-            print(ex)
+            if args.verbose:
+                print('fail to read json file!' + str(ex))
 
     return _jsons
 
@@ -33,10 +62,10 @@ def convert_to_Mbit(bitrate: int) -> float:
     return float(bitrate) / (1000 * 1000 / 8)
 
 
-def make_axis_elem(jsons:list, x_axis: list, y_axis_downspd: list, y_axis_upspd: list) -> None:
-    for json in jsons:
+def make_axis_elem(_jsons:list, x_axis: list, y_axis_downspd: list, y_axis_upspd: list) -> None:
+    for _json in _jsons:
         try:
-            date: str = json['timestamp']
+            date: str = _json['timestamp']
 
             from_date: datetime = datetime.strptime(
                 date,
@@ -44,19 +73,23 @@ def make_axis_elem(jsons:list, x_axis: list, y_axis_downspd: list, y_axis_upspd:
             )
             date_converted = from_date + timedelta(hours=KST)
 
-            down_speed: int = int(json['download']['bandwidth'])
-            up_speed: int = int(json['upload']['bandwidth'])
-            print('time : {0}, down : {1:.2f}Mbps, up : {2:.2f}Mbps'.format(
-                date_converted.strftime(local_format), convert_to_Mbit(down_speed), convert_to_Mbit(up_speed)))
+            down_speed: int = int(_json['download']['bandwidth'])
+            up_speed: int = int(_json['upload']['bandwidth'])
+
+            if args.verbose:
+                print('time : {0}, down : {1:.2f}Mbps, up : {2:.2f}Mbps'.format(
+                    date_converted.strftime(local_format), convert_to_Mbit(down_speed), convert_to_Mbit(up_speed)))
 
             x_axis.append(date_converted.strftime(local_format))
             y_axis_downspd.append(convert_to_Mbit(down_speed))
             y_axis_upspd.append((convert_to_Mbit(up_speed)))
         except Exception as ex:
-            print(ex)
+            if args.verbose:
+                print('fail to read data! ' + str(ex))
 
 
 if __name__ == '__main__':
+    args = make_parser()
 
     json_files: list = read_logs()
     json_files.sort()
@@ -68,6 +101,9 @@ if __name__ == '__main__':
     y_axis_upload_spd: list = []
 
     make_axis_elem(jsons, x_axis_timestamp, y_axis_download_spd, y_axis_upload_spd)
+
+    if args.csv:
+        save_as_file(date=x_axis_timestamp, up_speed=y_axis_upload_spd, down_speed=y_axis_download_spd)
 
     # plot graph
     y_download_smoothed = gaussian_filter1d(y_axis_download_spd, sigma=0.8)
